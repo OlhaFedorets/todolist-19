956b79fe9564f4481ebc6d3f37d783a513f703fc
OlhaFedorets <fedoretsolha@gmail.com>
2025-06-04 16:45:09 +0300

    change TodolistTitle.tsx

--- src/features/todolists/ui/Todolists/TodolistItem/TodolistTitle/TodolistTitle.tsx
+++ src/features/todolists/ui/Todolists/TodolistItem/TodolistTitle/TodolistTitle.tsx
@@ -1,36 +1,58 @@
-import { EditableSpan } from "@/common/components"
-import { useRemoveTodolistMutation, useUpdateTodolistTitleMutation } from "@/features/todolists/api/todolistsApi"
-import { type DomainTodolist } from "@/features/todolists/model/todolists-slice"
+import {EditableSpan} from "@/common/components"
+import {
+  todolistsApi,
+  useRemoveTodolistMutation,
+  useUpdateTodolistTitleMutation
+} from "@/features/todolists/api/todolistsApi"
+import {type DomainTodolist} from "@/features/todolists/model/todolists-slice"
 import DeleteIcon from "@mui/icons-material/Delete"
 import IconButton from "@mui/material/IconButton"
 import styles from "./TodolistTitle.module.css"
+import {RequestStatus} from "@/common/types";
+import {useAppDispatch} from "@/common/hooks";
 
 type Props = {
-  todolist: DomainTodolist
+    todolist: DomainTodolist
 }
 
-export const TodolistTitle = ({ todolist }: Props) => {
-  const { id, title, entityStatus } = todolist
+export const TodolistTitle = ({todolist}: Props) => {
+    const {id, title, entityStatus} = todolist
 
-  const [removeTodolist] = useRemoveTodolistMutation()
-  const [updateTodolistTitle] = useUpdateTodolistTitleMutation()
+    const [removeTodolist] = useRemoveTodolistMutation()
+    const [updateTodolistTitle] = useUpdateTodolistTitleMutation()
 
-  const deleteTodolist = () => {
-    removeTodolist(id)
-  }
+    const dispatch = useAppDispatch()
 
-  const changeTodolistTitle = (title: string) => {
-    updateTodolistTitle({ id, title })
-  }
+    const changeTodolistStatus = (entityStatus: RequestStatus) => {
+      dispatch(todolistsApi.util.updateQueryData('getTodolists', undefined, (state) => {
+        const todolist = state.find((todolist) => todolist.id === id)
+          if (todolist) {
+            todolist.entityStatus = entityStatus
+          }
+      }))
+    }
 
-  return (
-    <div className={styles.container}>
-      <h3>
-        <EditableSpan value={title} onChange={changeTodolistTitle} />
-      </h3>
-      <IconButton onClick={deleteTodolist} disabled={entityStatus === "loading"}>
-        <DeleteIcon />
-      </IconButton>
-    </div>
-  )
+    const deleteTodolist = () => {
+        changeTodolistStatus('loading')
+        removeTodolist(id)
+            .unwrap()
+            .catch(() => {
+            changeTodolistStatus('failed')
+        })
+    }
+
+    const changeTodolistTitle = (title: string) => {
+        updateTodolistTitle({id, title})
+    }
+
+    return (
+        <div className={styles.container}>
+            <h3>
+                <EditableSpan value={title} onChange={changeTodolistTitle}/>
+            </h3>
+            <IconButton onClick={deleteTodolist} disabled={entityStatus === "loading"}>
+                <DeleteIcon/>
+            </IconButton>
+        </div>
+    )
 }
