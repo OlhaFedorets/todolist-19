5813c10575e95a32e596ef8755d2d1383d908d2e
OlhaFedorets <fedoretsolha@gmail.com>
2025-06-05 17:43:14 +0300

    change baseApi.ts

--- src/app/baseApi.ts
+++ src/app/baseApi.ts
@@ -1,15 +1,29 @@
-import { AUTH_TOKEN } from "@/common/constants"
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+import {AUTH_TOKEN} from "@/common/constants"
+import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react"
+import {handleError} from "@/common/utils/handleError.ts";
 
 export const baseApi = createApi({
-  reducerPath: "todolistsApi",
-  tagTypes: ["Todolist", "Task"],
-  baseQuery: fetchBaseQuery({
-    baseUrl: import.meta.env.VITE_BASE_URL,
-    prepareHeaders: (headers) => {
-      headers.set("API-KEY", import.meta.env.VITE_API_KEY)
-      headers.set("Authorization", `Bearer ${localStorage.getItem(AUTH_TOKEN)}`)
+    reducerPath: "todolistsApi",
+    tagTypes: ["Todolist", "Task"],
+    // baseQuery: fetchBaseQuery({
+    //   baseUrl: import.meta.env.VITE_BASE_URL,
+    //   prepareHeaders: (headers) => {
+    //     headers.set("API-KEY", import.meta.env.VITE_API_KEY)
+    //     headers.set("Authorization", `Bearer ${localStorage.getItem(AUTH_TOKEN)}`)
+    //   },
+    // }),
+    baseQuery: async (args, api, extraOptions) => {
+        const result = await fetchBaseQuery({
+            baseUrl: import.meta.env.VITE_BASE_URL,
+            prepareHeaders: headers => {
+                headers.set('API-KEY', import.meta.env.VITE_API_KEY)
+                headers.set('Authorization', `Bearer ${localStorage.getItem(AUTH_TOKEN)}`)
+            },
+        })(args, api, extraOptions)
+
+        handleError(result, api)
+
+        return result
     },
-  }),
-  endpoints: () => ({}),
+    endpoints: () => ({}),
 })
